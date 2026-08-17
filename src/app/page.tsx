6230721b34727c0e3e65a34e7bf8988c060d5fa2
shayan2000r <Shayan2000r@gmail.com>
2026-08-17 13:49:12 +0200

    Redirect root to live app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { DashboardShell } from "@/components/dashboard-shell";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <DashboardShell />;
+  redirect("/app");
 }
